6cc73f2376ed8f83c01afd79ee706bdcd7d7bad9
Alexandr <124583216+Little5quad@users.noreply.github.com>
2023-03-20 13:50:19 +0300

    The final of third sprint

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -305,7 +305,56 @@ void TestRelevanceSort() {
     const string content_6 = "parrot barking like a dog in the village of dwarfes which is in the middle of elves forest"s;
     const vector<int> ratings_6 = {6, 7, 8};
  
-    {//Сортировка по убыванию релевантности, подсчёт рейтинга, относительное сравнение релевантности - порог +-0.001
+    {//Сортировка по убыванию релевантности, относительное сравнение релевантности 
+        SearchServer server;
+        //server.SetStopWords("in the"s);//stop words could interfere
+        server.AddDocument(doc_id_1, content_1, DocumentStatus::ACTUAL, ratings_1);
+        server.AddDocument(doc_id_2, content_2, DocumentStatus::ACTUAL, ratings_2);
+        server.AddDocument(doc_id_3, content_3, DocumentStatus::ACTUAL, ratings_3);
+        server.AddDocument(doc_id_4, content_4, DocumentStatus::ACTUAL, ratings_4);
+        server.AddDocument(doc_id_5, content_5, DocumentStatus::ACTUAL, ratings_5);
+        server.AddDocument(doc_id_6, content_6, DocumentStatus::ACTUAL, ratings_6);
+        const auto found_docs = server.FindTopDocuments("cat in the loan village"s);
+        ASSERT_EQUAL(found_docs.size(), 5);
+        const Document& doc0 = found_docs[0];
+        const Document& doc1 = found_docs[1];
+        const Document& doc2 = found_docs[2];
+        const Document& doc3 = found_docs[3];
+        const Document& doc4 = found_docs[4];
+        ASSERT_EQUAL(doc0.id, doc_id_4);
+        ASSERT(isRelativelyEqual(doc0.relevance, 0.56990995267518185, 1e-6));
+        ASSERT_EQUAL(doc1.id, doc_id_1);
+        ASSERT(isRelativelyEqual(doc1.relevance, 0.32023346136551606, 1e-6));
+        ASSERT_EQUAL(doc2.id, doc_id_3);
+        ASSERT(isRelativelyEqual(doc2.relevance, 0.16455769590499733, 1e-6));
+        ASSERT_EQUAL(doc3.id, doc_id_5);
+        ASSERT(isRelativelyEqual(doc3.relevance, 0.12835137028267893, 1e-6));
+        ASSERT_EQUAL(doc4.id, doc_id_6);
+        ASSERT(isRelativelyEqual(doc4.relevance, 0.075438604811010695, 1e-6));
+    }
+}
+
+void TestRating() {
+    const int doc_id_1 = 42;
+    const string content_1 = "cat in the city"s;
+    const vector<int> ratings_1 = {-1, -2, -3};
+    const int doc_id_2 = 24;
+    const string content_2 = "dog of a hidden village"s;
+    const vector<int> ratings_2 = {0, 3, 0};
+    const int doc_id_3 = 23;
+    const string content_3 = "silent assasin village cat in the village of darkest realms"s;
+    const vector<int> ratings_3 = {0, 4, 0};
+    const int doc_id_4 = 12;
+    const string content_4 = "cat of the loan village"s;
+    const vector<int> ratings_4 = {0, 5, 0};
+    const int doc_id_5 = 238;
+    const string content_5 = "parrot in the village of dwarfes"s;
+    const vector<int> ratings_5 = {0, 6, 0};
+    const int doc_id_6 = 9001;
+    const string content_6 = "parrot barking like a dog in the village of dwarfes which is in the middle of elves forest"s;
+    const vector<int> ratings_6 = {6, 7, 8};
+ 
+    {//подсчёт рейтинга
         SearchServer server;
         //server.SetStopWords("in the"s);//stop words could interfere
         server.AddDocument(doc_id_1, content_1, DocumentStatus::ACTUAL, ratings_1);
@@ -323,19 +372,14 @@ void TestRelevanceSort() {
         const Document& doc4 = found_docs[4];
         ASSERT_EQUAL(doc0.id, doc_id_4);
         ASSERT_EQUAL(doc0.rating, 1);
-        ASSERT(isRelativelyEqual(doc0.relevance, 0.56990995267518185, 1e-6));
         ASSERT_EQUAL(doc1.id, doc_id_1);
         ASSERT_EQUAL(doc1.rating, -2);
-        ASSERT(isRelativelyEqual(doc1.relevance, 0.32023346136551606, 1e-6));
         ASSERT_EQUAL(doc2.id, doc_id_3);
         ASSERT_EQUAL(doc2.rating, 1);
-        ASSERT(isRelativelyEqual(doc2.relevance, 0.16455769590499733, 1e-6));
         ASSERT_EQUAL(doc3.id, doc_id_5);
         ASSERT_EQUAL(doc3.rating, 2);
-        ASSERT(isRelativelyEqual(doc3.relevance, 0.12835137028267893, 1e-6));
         ASSERT_EQUAL(doc4.id, doc_id_6);
         ASSERT_EQUAL(doc4.rating, 7);
-        ASSERT(isRelativelyEqual(doc4.relevance, 0.075438604811010695, 1e-6));
     }
 }
  
@@ -440,6 +484,7 @@ void TestSearchStatusDocs() {
     const int doc_id_6 = 9001;
     const string content_6 = "parrot barking like a dog in the village of dwarfes which is in the middle of elves forest"s;
     const vector<int> ratings_6 = {6, 7, 8};
+    const double EPSILON = 1e-6;
     // Сначала убеждаемся, что поиск документа даёт результат с двумя строками
     {
         SearchServer server;
@@ -457,10 +502,10 @@ void TestSearchStatusDocs() {
             const Document& doc1 = found_docs[1];
             ASSERT_EQUAL(doc0.id, doc_id_4);
             ASSERT_EQUAL(doc0.rating, 1);
-            ASSERT(isRelativelyEqual(doc0.relevance, 0.6668070516454887, 1e-1));
+            ASSERT(isRelativelyEqual(doc0.relevance, 0.6668070516454887, EPSILON));
             ASSERT_EQUAL(doc1.id, doc_id_1);
             ASSERT_EQUAL(doc1.rating, 0);
-            ASSERT(isRelativelyEqual(doc1.relevance, 0.34657359027997264, 1e-1));
+            ASSERT(isRelativelyEqual(doc1.relevance, 0.34657359027997264, EPSILON));
         }
         {//Ищем забаненные
             const auto found_docs = server.FindTopDocuments("cat in the loan village"s, DocumentStatus::BANNED);
@@ -468,7 +513,7 @@ void TestSearchStatusDocs() {
             const Document& doc0 = found_docs[0];
             ASSERT_EQUAL(doc0.id, doc_id_2);
             ASSERT_EQUAL(doc0.rating, 1);
-            ASSERT(isRelativelyEqual(doc0.relevance, 0.03646431135879092, 1e-1));
+            ASSERT(isRelativelyEqual(doc0.relevance, 0.03646431135879092, EPSILON));
         }
         {//Ищем нерелевантные
             const auto found_docs = server.FindTopDocuments("cat in the loan village"s, DocumentStatus::IRRELEVANT);
@@ -477,10 +522,10 @@ void TestSearchStatusDocs() {
             const Document& doc1 = found_docs[1];
             ASSERT_EQUAL(doc0.id, doc_id_3);
             ASSERT_EQUAL(doc0.rating, 1);
-            ASSERT(isRelativelyEqual(doc0.relevance, 0.13222378676848182, 1e-1));
+            ASSERT(isRelativelyEqual(doc0.relevance, 0.13222378676848182, EPSILON));
             ASSERT_EQUAL(doc1.id, doc_id_6);
             ASSERT_EQUAL(doc1.rating, 7);
-            ASSERT(isRelativelyEqual(doc1.relevance, 0.013022968342425327, 1e-1));
+            ASSERT(isRelativelyEqual(doc1.relevance, 0.013022968342425327, EPSILON));
         }
         {//Ищем удалённые
             const auto found_docs = server.FindTopDocuments("cat in the loan village"s, DocumentStatus::REMOVED);
@@ -488,7 +533,7 @@ void TestSearchStatusDocs() {
             const Document& doc0 = found_docs[0];
             ASSERT_EQUAL(doc0.id, doc_id_5);
             ASSERT_EQUAL(doc0.rating, 2);
-            ASSERT(isRelativelyEqual(doc0.relevance, 0.045580389198488648, 1e-1));
+            ASSERT(isRelativelyEqual(doc0.relevance, 0.045580389198488648, EPSILON));
         }
     }
 }
@@ -500,6 +545,7 @@ void TestSearchServer() {
     RUN_TEST(TestMinusWords);
     RUN_TEST(TestMatchingWords);
     RUN_TEST(TestRelevanceSort);
+    RUN_TEST(TestRating);
     RUN_TEST(TestFuncDoumentFiltr);
     RUN_TEST(TestSearchStatusDocs);
     // Не забудьте вызывать остальные тесты здесь
