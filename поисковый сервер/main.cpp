84598fd0b3e2c6df9daaa7a1de6f40760dd3c094
Alexandr <124583216+Little5quad@users.noreply.github.com>
2023-04-16 23:23:38 +0300

    Update and rename search-server/main.cpp to поисковый сервер/main.cpp

--- поисковый сервер/main.cpp
+++ поисковый сервер/main.cpp
@@ -45,14 +45,6 @@ void MatchDocuments(const SearchServer& search_server, const string& query) {
     }
 }
 
-ostream& operator<<(ostream& out, const Document& document) {
-    out << "{ "s
-        << "document_id = "s << document.id << ", "s
-        << "relevance = "s << document.relevance << ", "s
-        << "rating = "s << document.rating << " }"s;
-    return out;
-}
-
 template <typename Iterator>
 ostream& operator<<(ostream& out, const IteratorRange<Iterator>& range) {
     for (Iterator it = range.begin(); it != range.end(); ++it) {
